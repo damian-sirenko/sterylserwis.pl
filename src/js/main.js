4f561ac16f41827f301b28087e107202a0d7c5ff
damian-sirenko <damian.sirenko@gmail.com>
2025-01-12 21:24:01 +0100

    about mob

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
-function setHeroHeight() {
-  const hero = document.querySelector('.container-header-hero');
-  hero.style.height = `${window.innerHeight}px`;
-}
+// function setHeroHeight() {
+//   const hero = document.querySelector('.container-header-hero');
+//   hero.style.height = `${window.innerHeight}px`;
+// }
 
-// Викликаємо функцію при завантаженні сторінки та зміні розміру вікна
-window.addEventListener('load', setHeroHeight);
-window.addEventListener('resize', setHeroHeight);
+// // Викликаємо функцію при завантаженні сторінки та зміні розміру вікна
+// window.addEventListener('load', setHeroHeight);
+// window.addEventListener('resize', setHeroHeight);
