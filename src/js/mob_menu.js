faeea37059ef2c2dba4848c824d1d5a3baadd0fd
damian-sirenko <damian.sirenko@gmail.com>
2025-01-18 09:37:39 +0100

    mob menu

---
+++ src/js/mob_menu.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector('.backdrop');
+const openMenuBtn = document.querySelector('.burger-menu');
+const closeMenuBtn = document.querySelector('.menu-close-btn');
+
+const openMenu = () => {
+  menu.classList.add('is-open');
+};
+
+const closeMenu = () => {
+  menu.classList.remove('is-open');
+};
+
+openMenuBtn.addEventListener('click', openMenu);
+closeMenuBtn.addEventListener('click', closeMenu);
