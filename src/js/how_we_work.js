1d4c4f11c43b099e07c54f30d6f5a426636b4c67
damian-sirenko <damian.sirenko@gmail.com>
2025-02-16 13:05:22 +0100

    how we work dt

--- src/js/how_we_work.js
+++ src/js/how_we_work.js
@@ -1,15 +1,8 @@
-document.querySelectorAll('.how-list-item').forEach(step => {
-  step.addEventListener('click', () => {
-    // Якщо крок відкритий, закриваємо його
-    if (step.classList.contains('open')) {
-      step.classList.remove('open');
-    } else {
-      // Закриваємо всі інші кроки
-      document.querySelectorAll('.how-list-item').forEach(otherStep => {
-        otherStep.classList.remove('open');
-      });
-      // Відкриваємо поточний крок
-      step.classList.add('open');
-    }
+document.querySelectorAll('.how-list-item').forEach(item => {
+  item.addEventListener('mouseenter', () => {
+    item.classList.add('open');
+  });
+  item.addEventListener('mouseleave', () => {
+    item.classList.remove('open');
   });
 });
