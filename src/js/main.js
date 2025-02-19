e8c5dea006fbf9aa638251ed9c62683541fe32c4
damian-sirenko <damian.sirenko@gmail.com>
2025-02-19 17:15:48 +0100

    123

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,1 @@
-// function setHeroHeight() {
-//   const hero = document.querySelector('.container-header-hero');
-//   hero.style.height = `${window.innerHeight}px`;
-// }
-
-// // Викликаємо функцію при завантаженні сторінки та зміні розміру вікна
-// window.addEventListener('load', setHeroHeight);
-// window.addEventListener('resize', setHeroHeight);
+import '../css/styles.css';
--- vite.config.js
+++ vite.config.js
@@ -4,54 +4,41 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig(({ command }) => {
-  return {
-    // Додаємо базовий шлях, щоб всі шляхи до ресурсів були відносними
-    base: '/',
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: chunkInfo => {
-            return '[name].js';
-          },
-          //  entryFileNames: chunkInfo => {
-          //    if (chunkInfo.name === 'commonHelpers') {
-          //      return 'commonHelpers.js';
-          //    }
-          //    return '[name].js';
-          //  },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
+export default defineConfig(({ command }) => ({
+  base: './',
+  define: {
+    [command === 'serve' ? 'global' : '_global']: {},
+  },
+  root: 'src',
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      input: glob.sync('./src/*.html'),
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+        entryFileNames: '[name].js',
+        assetFileNames: assetInfo => {
+          if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+            return '[name].[ext]';
+          }
+          return 'assets/[name]-[hash][extname]';
         },
       },
-      outDir: '../dist',
-      emptyOutDir: true,
     },
-    server: {
-      host: '127.0.0.1', // Локальний хост
-      port: 5173, // Стандартний порт Vite
-    },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/*.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
-    ],
-  };
-});
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+  },
+  plugins: [
+    injectHTML(),
+    FullReload(['./src/**/*.html']),
+    SortCss({ sort: 'mobile-first' }),
+  ],
+}));
